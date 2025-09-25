5e7cd385a006fe3f14687b197720496d38ab594e
ayanisbatman <bariwala@ualberta.ca>
2025-09-24 18:57:19 -0600

    Update Circle.java

--- app/src/main/java/com/example/binary1/Circle.java
+++ app/src/main/java/com/example/binary1/Circle.java
@@ -3,19 +3,28 @@ package com.example.binary1;
 public class Circle extends Shape {
     private double radius;
 
-
-    public Circle(int x, int y, double radius) {
-        super(x, y);
+    // Constructor
+    public Circle(double radius) {
         this.radius = radius;
     }
 
-    public double getRadius() { return radius; }
-    public void setRadius(double radius) { this.radius = radius; }
-
+    @Override
+    public void draw() {
+        System.out.println("Drawing a Circle with radius " + radius);
+    }
 
     @Override
     public double getArea() {
         return Math.PI * radius * radius;
     }
 
+    // Getter
+    public double getRadius() {
+        return radius;
+    }
+
+    // Setter
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
 }
